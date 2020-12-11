d64fd4abbeaf52236bab35f789cef23bd1ffa07d
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2020-12-11 20:47:58 +0000

    vv

--- lib/config.dart
+++ lib/config.dart
@@ -3,7 +3,7 @@ var config = {
     'facebook': {
       'appId': '825097184718982',
       'appSecret': 'd0dc963615d9831dfeefaa412e39118d',
-      'url': 'https://postit-ui.herokuapp.com/#/dashboard/facebook'
+      'url': 'https://postit-ui.herokuapp.com/%23/dashboard/facebook'
     }
   },
 };
