e54d6d2c4600d27cc5ad0c9b0b777dbe08a09c28
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2020-12-26 12:26:54 +0000

    fixed the index.html issue

--- bin/main.dart
+++ bin/main.dart
@@ -1,28 +1,52 @@
-import 'dart:io' show File, HttpServer, InternetAddress, Platform;
+//import 'dart:developer';
+//import 'dart:io' show File, HttpServer, InternetAddress, Platform;
 import 'dart:async';
-import 'package:http_server/http_server.dart';
-import 'package:path/path.dart' show join, dirname;
-import 'package:shelf/shelf_io.dart' as io;
-import 'package:shelf/shelf.dart' as shelf;
+import 'dart:io' show Platform;
+import 'package:angel_framework/angel_framework.dart';
+import 'package:angel_framework/http.dart';
+import 'package:angel_static/angel_static.dart';
+import 'package:file/local.dart';
+import 'package:logging/logging.dart';
 
-void main() {
+void main(List<String> args) {
 
-  var pathToBuild = join(dirname(Platform.script.toFilePath()), '..', 'build');
-  var staticFiles = VirtualDirectory(pathToBuild);
+  var app = Angel();
+  var http = AngelHttp(app);
+  var fs = const LocalFileSystem();
+  var vDir = CachingVirtualDirectory(
+    app,
+    fs,
+    allowDirectoryListing: true,
+    source: fs.directory('./build') /*args.isEmpty ? fs.currentDirectory : fs.directory(args[0])*/,
+    maxAge: const Duration(days: 24).inSeconds,
+  );
 
-  staticFiles.followLinks = true;
-  staticFiles.allowDirectoryListing = true;
-  staticFiles.directoryHandler = (dir, req) {
-    var indexUri = Uri.file(dir.path).resolve('index.html');
-    staticFiles.serveFile(File(indexUri.toFilePath()), req);
-  };
+  app.mimeTypeResolver
+    ..addExtension('', 'text/html')
+    ..addExtension('dart', 'text/dart')
+    ..addExtension('lock', 'text/plain')
+    ..addExtension('markdown', 'text/plain')
+    ..addExtension('md', 'text/plain')
+    ..addExtension('yaml', 'text/plain');
+//    ..addEx
+
+  app.logger = Logger('server.log')
+    ..onRecord.listen((rec) {
+      print(rec);
+      if (rec.error != null) print(rec.error);
+      if (rec.stackTrace != null) print(rec.stackTrace);
+    });
+
+  app.fallback(vDir.handleRequest);
+  app.fallback(vDir.pushState('index.html'));
+
 
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 9939 : int.parse(portEnv);
 
   runZoned(() async {
-    var server = await HttpServer.bind('0.0.0.0', port);
-    print('Serving on ${server.address.host}:${server.port}');
-    await server.forEach(staticFiles.serveRequest);
+    var server = await http.startServer('0.0.0.0', port);
+    print('Serving from ${vDir.source.path}');
+    print('Listening at http://${server.address.address}:${server.port}');
   }, onError: (e, stackTrace) => print('Oh noes! $e $stackTrace'));
 }
