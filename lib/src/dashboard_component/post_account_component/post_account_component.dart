d4ef2651632292a7b23ff354c0017a34fc263289
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2021-01-23 12:36:44 +0000

    jhj

--- lib/src/dashboard_component/post_account_component/post_account_component.dart
+++ lib/src/dashboard_component/post_account_component/post_account_component.dart
@@ -130,7 +130,7 @@ class PostAccountComponent implements OnInit {
     var appId = fbConfig['appId'];
     var url = fbConfig['url'];
 
-    loginLinkUrl = 'https://www.facebook.com/v9.0/dialog/oauth/?display=popup&client_id=$appId&redirect_uri=$url&state=access_token&scope=pages_read_engagement,pages_show_list,pages_manage_posts';
+    loginLinkUrl = 'https://www.facebook.com/v9.0/dialog/oauth/?display=popup&client_id=$appId&redirect_uri=$url&state=access_token&scope=id,name';
   }
 
   @override
