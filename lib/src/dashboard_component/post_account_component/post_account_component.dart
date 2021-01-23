20f6ed38c5c4a4647c8c24b5ce5504c9e793fcf0
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2021-01-23 13:12:52 +0000

    gtt

--- lib/src/dashboard_component/post_account_component/post_account_component.dart
+++ lib/src/dashboard_component/post_account_component/post_account_component.dart
@@ -131,6 +131,7 @@ class PostAccountComponent implements OnInit {
     var url = fbConfig['url'];
 
     loginLinkUrl = 'https://www.facebook.com/v9.0/dialog/oauth/?display=popup&client_id=$appId&redirect_uri=$url&state=access_token';
+    
   }
 
   @override
