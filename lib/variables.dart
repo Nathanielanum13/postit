d1d026d6802b5ede9d2752c21b393ec3f00152d4
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2021-02-07 21:49:23 +0000

    changess

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,4 +1,4 @@
-Map env = const {
+/*Map env = const {
    'POST_URL': 'http://localhost:5379/posts',
    'SCHEDULE_URL': 'http://localhost:5379/schedule-post',
    'BATCH_DELETE_URL': 'http://localhost:5379/batch-delete',
@@ -10,19 +10,18 @@ Map env = const {
    'SCHEDULE_COUNT_URL': 'http://localhost:5379/count/schedule',
    'SCHEDULE_STATUS_WEBSOCKET': 'ws://localhost:5379/pws/schedule-status',
    'MEDIA_UPLOAD_URL': 'http://localhost:5379/file/upload'
+};*/
+
+Map env = const {
+ 'POST_URL': 'https://postit-backend-api.herokuapp.com/posts',
+ 'SCHEDULE_URL': 'https://postit-backend-api.herokuapp.com/schedule-post',
+ 'BATCH_DELETE_URL': 'https://postit-backend-api.herokuapp.com/batch-delete',
+ 'LOGIN_URL': 'https://postit-auth.herokuapp.com/login',
+ 'SIGNUP_URL': 'https://postit-auth.herokuapp.com/signup',
+ 'VALIDATE_TOKEN_URL': 'https://postit-auth.herokuapp.com/validate',
+ 'FACEBOOK_URL': 'https://postit-backend-api.herokuapp.com/fb/code',
+ 'DELETE_FACEBOOK_ACCOUNT_URL': 'https://postit-backend-api.herokuapp.com/fb/code',
+ 'COUNT_URL': 'https://postit-backend-api.herokuapp.com/count/data',
+ 'SCHEDULE_STATUS_WEBSOCKET': 'wss://postit-backend-api.herokuapp.com/pws/schedule-status',
+'MEDIA_UPLOAD_URL': 'https://postit-backend-api.herokuapp.com/file/upload'
 };
-
-//Map env = const {
-// 'POST_URL': 'https://postit-backend-api.herokuapp.com/posts',
-// 'SCHEDULE_URL': 'https://postit-backend-api.herokuapp.com/schedule-post',
-// 'BATCH_DELETE_URL': 'https://postit-backend-api.herokuapp.com/batch-delete',
-// 'LOGIN_URL': 'https://postit-auth.herokuapp.com/login',
-// 'SIGNUP_URL': 'https://postit-auth.herokuapp.com/signup',
-// 'VALIDATE_TOKEN_URL': 'https://postit-auth.herokuapp.com/validate',
-// 'FACEBOOK_URL': 'https://postit-backend-api.herokuapp.com/fb/code',
-// 'DELETE_FACEBOOK_ACCOUNT_URL': 'https://postit-backend-api.herokuapp.com/fb/code',
-// 'COUNT_URL': 'https://postit-backend-api.herokuapp.com/count/data',
-// 'SCHEDULE_STATUS_WEBSOCKET': 'wss://postit-backend-api.herokuapp.com/pws/schedule-status',
-//'MEDIA_UPLOAD_URL': 'https://postit-backend-api.herokuapp.com/file/upload'
-
-//};
