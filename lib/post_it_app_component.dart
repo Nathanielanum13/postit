bc871201a6f403a904e2d5611398631226759bf8
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2021-02-08 01:16:54 +0000

    changed from localStorage to sessionStorage

--- lib/post_it_app_component.dart
+++ lib/post_it_app_component.dart
@@ -3,12 +3,6 @@ import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular_app/src/routes.dart';
 import 'package:angular_router/angular_router.dart';
-import 'package:angular_app/src/home_component/home_component.dart' deferred as home_page;
-import 'package:angular_app/src/about_component/about_component.dart' deferred as about_page;
-import 'package:angular_app/src/login_component/login_component.dart' deferred as login_page;
-import 'package:angular_app/src/signup_component/signup_component.dart' deferred as signup_page;
-import 'package:angular_app/src/dashboard_component/dashboard_component.dart' deferred as dashboard_page;
-
 
 @Component(
   selector: 'postit-app',
@@ -20,11 +14,7 @@ import 'package:angular_app/src/dashboard_component/dashboard_component.dart' de
 class PostItAppComponent implements OnInit {
   @override
   Future<void> ngOnInit() async {
-    await home_page.loadLibrary();
-    await about_page.loadLibrary();
-    await login_page.loadLibrary();
-    await signup_page.loadLibrary();
-    await dashboard_page.loadLibrary();
+    print("Thank you for using POSTIT");
   }
 }
 
