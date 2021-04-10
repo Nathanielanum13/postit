01d4e34122f5a096dd191b684cf2d676d1a5c6e1
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2021-04-10 20:58:45 +0000

    nn

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,7 +1,7 @@
 //development
-/*const base_url = 'http://localhost:5379';
-const base_auth_url = 'http://localhost:3576';
-const base_socket_url = 'ws://localhost:3576';*/
+// const base_url = 'http://localhost:5379';
+// const base_auth_url = 'http://localhost:3576';
+// const base_socket_url = 'ws://localhost:3576';
 
 //production
 const base_url = 'https://postit-dev-api.herokuapp.com';
@@ -18,6 +18,7 @@ Map env = const {
  'VALIDATE_TOKEN_URL': '$base_auth_url/validate',
  'FACEBOOK_URL': '$base_url/all/code',
  'DELETE_FACEBOOK_ACCOUNT_URL': '$base_url/fb/code',
+ 'ALL_ACCOUNTS_URL': '$base_url/fb/code',
  'COUNT_URL': '$base_url/count/data',
  'SCHEDULE_STATUS_WEBSOCKET': '$base_socket_url/pws/schedule-status',
  'MEDIA_UPLOAD_URL': '$base_url/file/upload',
