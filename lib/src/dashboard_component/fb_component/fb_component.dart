e1a93bd4362593f0449a325e621159fc4c4fdad3
Nathaniel Anum Adjah <nathanielanum13@gmail.com>
2021-04-16 08:16:41 +0100

    launching day

--- lib/src/dashboard_component/fb_component/fb_component.dart
+++ lib/src/dashboard_component/fb_component/fb_component.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:html';
 
 import 'package:angular/angular.dart';
 import 'package:angular_app/src/dashboard_component/dashboard_services/facebook_data_service.dart';
@@ -12,16 +13,16 @@ import 'package:angular_router/angular_router.dart';
   exports: [InnerRoutePaths, InnerRoutes],
   providers: [ClassProvider(FacebookDataService)],
 )
-class FbComponent extends OnActivate {
+class FbComponent extends OnActivate implements OnInit {
   String code;
   final Router _router;
   final FacebookDataService _facebookDataService;
+  var appTheme;
 
   FbComponent(this._facebookDataService, this._router);
 
   @override
   Future<void> onActivate(RouterState previous, RouterState current) async {
-
     // Make sure the code parameter is not empty
     if (current.queryParameters['code'] == '') {
       print('Something went wrong');
@@ -30,7 +31,6 @@ class FbComponent extends OnActivate {
 
     // If it isn't empty store it in the code variable
     code = current.queryParameters['code'];
-    print('Code: ${code}');
 
     // Send the code to the service to send the data
     var response = await _facebookDataService.sendCodeToApi(code);
@@ -38,25 +38,12 @@ class FbComponent extends OnActivate {
       return;
     }
 
-    print('Json Response Body: ${json.decode(response.body)}');
-//    var fbConfig = config['authentication']['facebook'];
-//    var appId = fbConfig['appId'];
-//    var url = fbConfig['url'];
-//    var appSecret = fbConfig['appSecret'];
-//
-//    var link = 'https://graph.facebook.com/oauth/access_token?client_id=$appId&redirect_uri=$url&client_secret=$appSecret&code=$_code';
-//    http.read(link).then((value) {
-//      var uri = Uri.splitQueryString(value);
-//      var acc = uri['access_token'];
-//      print(acc);
-//
-//      http.read('https://graph.facebook.com/me?access_token=$acc').then((contents) {
-//        var user =json.decode(contents);
-//
-//        print(user); // Logged in as this user.
-//      });
-//    });
     _router.navigate(InnerRoutePaths.post_account.toUrl());
 
+  }
+
+  @override
+  void ngOnInit() {
+    appTheme = json.decode(window.localStorage['x-user-preference-theme']);
   }
 }
